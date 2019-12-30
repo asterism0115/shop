aab8ccd5a12ace9a16970eca8988c6fdb4895a36
Chris Luo <chris.ying.luo@gmail.com>
2019-12-29 19:44:47 -0800

    added products overview screen

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FlatList, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
+
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      renderItem={itemData => (
+        <ProductItem
+          title={itemData.item.title}
+          image={itemData.item.imageUrl}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
+};
+
+export default ProductsOverviewScreen;
