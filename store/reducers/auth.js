33543f6dcbcb2d3d186c3bcb8c546439b0f01c3f
Chris Luo <chris.ying.luo@gmail.com>
2020-01-08 17:52:37 -0800

    added auto login and auto logout

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,4 +1,4 @@
-import { LOGIN, SIGNUP } from '../actions/auth';
+import { AUTHENTICATE, LOGOUT } from '../actions/auth';
 
 const initialState = {
   token: null,
@@ -7,18 +7,14 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN:
+    case AUTHENTICATE:
       return {
         ...state,
         token: action.token,
         userId: action.userId
       };
-    case SIGNUP:
-      return {
-        ...state,
-        token: action.token,
-        userId: action.userId
-      };
+    case LOGOUT:
+      return initialState;
     default:
       return state;
   }
